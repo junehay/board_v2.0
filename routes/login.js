2da0d88da18d608523e3d2eb10f50b4d6b14a3b4
junehay <ndrun@naver.com>
2019-09-04 23:44:31 +0900

    mariaDB

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const passport = require('./login_passport');
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+    host: 'localhost',
+    port : 3306,
+    user : 'June',
+    password : 'hay',
+    database : 'user'
+});
 
 // login
 router.get('/', function(req, res){
@@ -29,18 +38,39 @@ router.get('/logout', function(req, res){
 });
 
 // signup
-router.get('/signup', function(req,res){
+router.get('/signup', function(req, res){
     res.render('signup/signup.html');
 });
-router.post('/signup', function(req,res){
+
+/*router.post('/signup', function(req, res){
     const userid = req.body.userid;
-    User.findOne({userid:userid}, function(err, user){
+    sequelize.findOne({userid:userid}, function(err, user){
         if(!user){
-            return User.create(req.body, function(err, user){
+            return sequelize.create(req.body, function(err, user){
             if(err) return res.json(err);
             res.send('<script>alert("Sign up is complete");location.href="/";</script>')});}
             else{return res.send('<script>alert("아이디 중복");location.href="/signup";</script>');}
     });
+});*/
+
+router.post('/signup', function(req, res){
+    try{
+        const id = req.body.userid;
+        const password = req.body.password;
+        const name = req.body.name;
+
+        pool.query('SELECT * FROM user WHERE userid= OR name=?', [id, name], function(err, data){
+            if(data.length == 0){
+                pool.query('INSERT INTO user VALUES(?,?,?)', [id, password, name], function(){
+                    console.log(id, password, name);
+                    res.redirect('/signup');
+                });
+            }
+        });
+            
+    }catch(err){
+        console.log(err);
+        }   
 });
 
 module.exports = router;
